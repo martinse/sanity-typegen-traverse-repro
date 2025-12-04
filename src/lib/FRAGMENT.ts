1021b2be5a7d958e2f3ef94b9f99d470504e77f5
martin <m@psb.sh>
2025-12-04 13:41:28 +0100

    as string test

--- src/lib/FRAGMENT.ts
+++ src/lib/FRAGMENT.ts
@@ -4,4 +4,4 @@ export const FRAGMENT = `
     "alt": select(defined(alt) => alt, defined(asset->altText) => asset->altText),
     "lqip": select(defined(asset->metadata.lqip) => asset->metadata.lqip),
 }
-`
+` as string
